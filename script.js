2f737bbdda658fd5a44b68ce049711a058a299ca
vissage <smughatkid71@gmail.com>
2024-10-28 12:56:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button")
 
 buttons.forEach((btn) => (
+
+    display.value = eval(0),
+
     btn.addEventListener("click", () =>{
+            if(display.value === "0"){
+                display.value = display.value.slice(0 ,-1);
+            }
+        
             if(btn.id === "="){
                 display.value = eval(display.value)    
             } else if ( btn.id === "AC") {
@@ -10,6 +17,10 @@ buttons.forEach((btn) => (
             } else if (btn.id == "DE"){
                 display.value = display.value.slice(0, -1);
             } else display.value += btn.id
+
+            if(display.value === ""){
+                display.value = eval(0);
+            }
 
     } )
 ) )
